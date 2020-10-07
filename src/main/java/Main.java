b00a34991fe19af6ed19644c43fa3fe6d99089ff
DUCROUX <jacques.ducroux@gmail.com>
2020-10-07 15:49:15 +0200

    Exercice 002 : Création de la méthode primeAnuelleBase réussi

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+        public static void main(String[] args)
+        {
+            String myString = "Hello";
+            Integer number = 4;
+            System.out.println("Hello World");
+            System.out.println("toto");
+
+        }
+}
